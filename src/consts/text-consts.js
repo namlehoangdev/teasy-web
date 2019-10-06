1d074be9513adcecdc8c82b2b250356ca800bc85
kiennm <kiennm@vng.com.vn>
2019-10-06 19:51:59 +0700

    UAT text

--- src/consts/text-consts.js
+++ src/consts/text-consts.js
@@ -1,9 +1,10 @@
 const APP_NAME = 'TEASY';
 const APP_SHORT_DESCRIPTION = 'Nền tảng tạo cuộc thi online';
-const APP_DESCRIPTION = 'Tạo cuộc thi, đề thi, ngân hàng câu hỏi trực tuyến. Tham gia vào các cuộc thi bổ ích của cộng đồng hoặc cá nhân, tổ chức';
-const ROOM_CODE = 'Mã phòng thi';
+const APP_DESCRIPTION = 'Tổ chức, quản lý các cuộc thi trực tuyến. Tham gia dự thi dễ dàng, nhanh chóng';
+const OR = 'hoặc';
+const ROOM_CODE = 'Nhập mã thi nhanh..';
 const I_AGREE = 'Tôi đồng ý';
-const ENTER_ROOM = 'Vào phòng thi';
+const ENTER_ROOM = 'Thi nhanh';
 const LOGIN_WITH_FACEBOOK = 'Đăng nhập bằng Facebook';
 const GOTO_ADMIN_PAGE = 'Tạo cuộc thi';
 const GOTO_PLAYGROUND = 'Tham gia thi';
@@ -11,14 +12,15 @@ const GO_BACK = 'Quay lại';
 
 
 export const TEXT = {
-    appName: APP_NAME,
-    appShortDescription: APP_SHORT_DESCRIPTION,
-    appDescription: APP_DESCRIPTION,
-    roomCode: ROOM_CODE,
-    iAgree: I_AGREE,
-    enterRoom: ENTER_ROOM,
-    loginWithFacebook: LOGIN_WITH_FACEBOOK,
-    gotoAdmin: GOTO_ADMIN_PAGE,
-    gotoPlayground: GOTO_PLAYGROUND,
-    goBack: GO_BACK
+  appName: APP_NAME,
+  appShortDescription: APP_SHORT_DESCRIPTION,
+  appDescription: APP_DESCRIPTION,
+  roomCode: ROOM_CODE,
+  iAgree: I_AGREE,
+  enterRoom: ENTER_ROOM,
+  loginWithFacebook: LOGIN_WITH_FACEBOOK,
+  gotoAdmin: GOTO_ADMIN_PAGE,
+  gotoPlayground: GOTO_PLAYGROUND,
+  goBack: GO_BACK,
+  or: OR
 };
